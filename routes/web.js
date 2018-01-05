1cb12e4fda4f4bcc031ed91bdeaca99f3f9be06c
Alpha Man <smartclashcoc@gmail.com>
2018-01-05 23:28:51 +0530

    Let's push some code

--- routes/web.js
+++ routes/web.js
@@ -8,4 +8,9 @@ route.post('/auth/login', AuthController.login);
 route.post('/auth/register', AuthController.register);
 route.post('/auth/logout', AuthController.logout);
 
+route.get('/', (req, res) => {
+	res.send('Some kind of error');
+	throw new Error('Aww shit. You broke something');
+});
+
 module.exports = {route};
