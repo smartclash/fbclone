091650acd08adf133220223c558271930ea87601
Alpha Man <smartclashcoc@gmail.com>
2018-01-05 23:42:20 +0530

    Last time, or throw sentry shit

--- bootstrap/bootstrap.js
+++ bootstrap/bootstrap.js
@@ -7,8 +7,9 @@ const app = express();
 raven.config('https://66d9e1dd671b4e139bb7932772fafc4f@sentry.io/267471').install();
 
 app.use(raven.requestHandler());
-app.use(raven.errorHandler());
 app.use(bodyParser.json());
 app.use(require('./../routes/web').route);
+
+app.use(raven.errorHandler());
 
 module.exports = {app};
