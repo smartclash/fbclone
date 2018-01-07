d1009643a78cf5f03298902b59ae6eb1c329d6e7
Alpha Man <smartclashcoc@gmail.com>
2018-01-07 16:51:35 +0530

    More proper error reporting

--- app/Http/Controllers/users-controller.js
+++ app/Http/Controllers/users-controller.js
@@ -3,11 +3,27 @@ const userRequests = require('./../Requests/user-requests');
 const usersInfo = (req, res) => {
 	userRequests.userInfo(req.token)
 		.then(response => res.status(200).send(response))
-		.catch(err => res.status(401).send({
-			type: 'error',
-			message: 'You are either not logged in or an error occured',
-			token: req.token
-		}));
+		.catch(err => {
+			let error;
+			if (err.response.data.code === 'unauthorized') {
+				error = {
+					type: 'error',
+					message: 'You are not authenticated'
+				};
+			} else if (err.response.data.message === 'invalid authorization token') {
+				error = {
+					type: 'error',
+					message: 'The token is invalid'
+				};
+			} else {
+				error = {
+					type: 'error',
+					message: 'Unknown error occured. Contact the developer'
+				};
+			}
+
+			res.status(401).send(error);
+		});
 };
 
 module.exports = {usersInfo};
